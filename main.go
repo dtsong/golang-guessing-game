be1a97cba0c1edcc60d4367e96a78bcc08be5f40
Daniel Song <xdtsong@gmail.com>
2021-09-04 10:42:21 -0700

    modify initial seed for the rand generator

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 	min, max := 1, 100
+	rand.Seed(time.Now().UnixNano())
 	secretNumber := rand.Intn(max-min) + min
 	fmt.Println("The secret number is", secretNumber)
 }
