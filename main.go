fa77075da6f0dc4c998691f5a52d7040abc8aeda
Daniel Song <xdtsong@gmail.com>
2021-09-04 10:53:22 -0700

    Add libraries for user input

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,5 +14,25 @@ func main() {
 	min, max := 1, 100
 	rand.Seed(time.Now().UnixNano())
 	secretNumber := rand.Intn(max-min) + min
+
+	fmt.Println("Guess a number between 1 and 100")
+	fmt.Println("Please input your guess")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Reading Input Error", err)
+		return
+	}
+
+	input = strings.TrimSuffix(input, "\n")
+
+	guess, err := strconv.Atoi(input)
+
+	if err != nil {
+		fmt.Println("Invalid Input, must be an integer", err)
+	}
+
+	fmt.Println("Your guess is", guess)
 	fmt.Println("The secret number is", secretNumber)
 }
